7e821566ebb76826c38a2628db92ec69593a00b2
Changhoon Jin <susemi99@gmail.com>
2014-04-16 20:30:54 +0900

    all

---
+++ sender.rb
@@ -0,0 +1,34 @@
+require 'sinatra'
+require 'gcm'
+
+# for production mode
+# set :port, 3002
+# set :environment, :production
+
+get '/' do
+	erb :index, :locals => {apiKey: '', regId: '', msg: ''}
+end
+
+put '/' do
+	apiKey = params[:apiKey] || ''
+	regIdStr = params[:regId] || ''
+	msg = params[:msg] || ''
+
+	if (!apiKey.empty? && !regIdStr.empty? && !msg.empty?)
+	begin
+		begin
+			gcm = GCM.new(apiKey)
+			regIDs= regIdStr.split(',')
+			options = {data: JSON.parse(msg)}
+			response = gcm.send_notification(regIDs, options)
+			result = JSON.pretty_generate(response)		
+		rescue => error
+			result = error
+		end
+	end
+	else
+		result = "error : fill all field"
+	end
+
+	erb :index, :locals => {apiKey: apiKey, regId: regIdStr, msg: msg, response: result }
+end
